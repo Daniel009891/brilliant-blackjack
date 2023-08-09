23ce4d541dd9b75ea84881e493b068cd93ffc0a7
Daniel009891 <>
2023-08-09 12:40:16 +0000

    Initial commit with game introduction function started

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def game_introduction():
+    """
+    Displays a welcome message for the user.
+    Asks if the user would like to read game instructions
+    or play game.
+    """
+    print("game introduction")
+
+game_introduction()
