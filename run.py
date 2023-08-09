a1344d1db49d4abbdf579fb457d2ed0fe08b828f
Daniel009891 <>
2023-08-09 15:10:34 +0000

    Add while loop to game rules selection

--- run.py
+++ run.py
@@ -10,13 +10,14 @@ def game_introduction():
     Asks if the user would like to read game instructions
     or play game.
     """
+    # player name input
     print(pyfiglet.figlet_format("WELCOME TO\n BRILLIANT BLACKJACK"))
     time.sleep(1)
     while True:
         name = typingInput("\nWelcome! please enter your name:\n").capitalize()
         if (name != "Computer") and (name != ""):
             print("\n")
-            typingPrint(f"Welcome {name}!")
+            typingPrint(f"Welcome {name}!\n")
             break
 
         elif name == "Computer":
@@ -25,7 +26,17 @@ def game_introduction():
         
         else: typingPrint("\nSorry you didnt input a name! please enter a valid name!\n")
         print("\n")
-    
+    # Game rules input
+    while True:
+        choices = typingInput("Would you like to read the rules of the game? (Type Y for yes and N for no):").lower()
+        if choices == "y":
+            typingPrint("I see.... you need a little help....here you go!")
+            game_rules()
+        elif choices == "n" :
+            typingPrint("You like to live dangerously.... I like it! lets play!!!")
+            start_game()
+        else:
+            typingPrint("Sorry you can only select Y for (yes) or N for (no), please try again")
 
 
 def typingPrint(text):
