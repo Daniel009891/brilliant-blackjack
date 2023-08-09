4a1b5054f7cd1fb0257a7ce0ddf0b67752303fa0
Daniel009891 <>
2023-08-09 15:35:04 +0000

    Add new lines to finctions and code credits to borrowed code

--- run.py
+++ run.py
@@ -21,29 +21,35 @@ def game_introduction():
         name = typingInput("\nWelcome! please enter your name:\n").capitalize()
         if (name != "Computer") and (name != ""):
             print("\n")
-            typingPrint(f"Welcome {name}!\n")
+            print(pyfiglet.figlet_format(f"Welcome {name}!\n"))
             break
 
         elif name == "Computer":
             typingPrint("\nSorry you cannot be the computer! please enter a valid name!\n")
             print("\n")
         
-        else: typingPrint("\nSorry you didnt input a name! please enter a valid name!\n")
+        else: typingPrint("\nSorry you didn't input a name! please enter a valid name!\n")
         print("\n")
     # Game rules input
+    time.sleep(1)
     while True:
-        choices = typingInput("Would you like to read the rules of the game? (Type Y for yes and N for no):").lower()
+        print("\n")
+        choices = typingInput("\nWould you like to read the rules of the game? (Type Y for yes and N for no):").lower()
         if choices == "y":
             typingPrint("I see.... you need a little help....here you go!")
+            time.sleep(1)
             game_rules()
         elif choices == "n" :
             typingPrint("You like to live dangerously.... I like it! lets play!!!")
+            time.sleep(1)
             start_game()
         else:
-            typingPrint("Sorry you can only select Y for (yes) or N for (no), please try again")
+            typingPrint("\n Sorry you can only select Y for (yes) or N for (no), please try again!\n")
+            print("\n")
 
 
 def typingPrint(text):
+    #code inspired by https://www.101computing.net/python-typing-text-effect/
     for character in text:
         sys.stdout.write(character)
         sys.stdout.flush()
@@ -51,6 +57,7 @@ def typingPrint(text):
 
 
 def typingInput(text):
+    #code inspired by https://www.101computing.net/python-typing-text-effect/
     for character in text:
         sys.stdout.write(character)
         sys.stdout.flush()
