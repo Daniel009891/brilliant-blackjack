52d214c9ed5b89c282bd33063fc3de1553c6ca5f
Daniel009891 <>
2023-08-13 14:44:55 +0000

    Added error message for blank selection to game rules section of game introduction function

--- run.py
+++ run.py
@@ -4,13 +4,14 @@ import time
 import sys
 
 suits = ["Diamonds", "Clubs", "Hearts", "Spades"]
-cards = ["2","3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
 values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
 
 
 def game_introduction():
     """
     Displays a welcome message for the user.
+    Asks user to input a name.
     Asks if the user would like to read game instructions
     or play game.
     """
@@ -27,8 +28,8 @@ def game_introduction():
         elif name == "Computer":
             typingPrint("\nSorry you cannot be the computer! please enter a valid name!\n")
             print("\n")
-        
-        else: typingPrint("\nSorry you didn't input a name! please enter a valid name!\n")
+        else: 
+            typingPrint("\nSorry you didn't input a name! please enter a valid name!\n")
         print("\n")
     # Game rules input
     time.sleep(1)
@@ -36,20 +37,26 @@ def game_introduction():
         print("\n")
         choices = typingInput("\nWould you like to read the rules of the game? (Type Y for yes and N for no):").lower()
         if choices == "y":
+            print("\n")
             typingPrint("I see.... you need a little help....here you go!")
             time.sleep(1)
             game_rules()
-        elif choices == "n" :
+        elif choices == "n":
+            print("\n")
             typingPrint("You like to live dangerously.... I like it! lets play!!!")
             time.sleep(1)
             start_game()
+        elif choices == "":
+            typingPrint("\n Sorry no selection was made, please select Y for (yes) or N for (no), please try again!\n")
+            print("\n")
+            time.sleep(1)
         else:
             typingPrint("\n Sorry you can only select Y for (yes) or N for (no), please try again!\n")
             print("\n")
 
 
 def typingPrint(text):
-    #code inspired by https://www.101computing.net/python-typing-text-effect/
+    # code inspired by https://www.101computing.net/python-typing-text-effect/
     for character in text:
         sys.stdout.write(character)
         sys.stdout.flush()
@@ -57,7 +64,7 @@ def typingPrint(text):
 
 
 def typingInput(text):
-    #code inspired by https://www.101computing.net/python-typing-text-effect/
+    # code inspired by https://www.101computing.net/python-typing-text-effect/
     for character in text:
         sys.stdout.write(character)
         sys.stdout.flush()
